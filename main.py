23bad74d8e9a63e83b9a29e195e592037b8ccc92
C Lewis Messina <33812538+CLewisMessina@users.noreply.github.com>
2025-04-28 07:02:59 -0500

    Add logo, change UI to Simplex theme
    Added logo image, altered main.py to accept logo and change UI to ttkbootstratp "Simplex" theme.

--- main.py
+++ main.py
@@ -2,12 +2,16 @@
 import ttkbootstrap as ttkb
 from ttkbootstrap.constants import *
 from ui.app_frame import AppFrame
+import tkinter as tk  # <-- Added to handle the app icon
 
 def main():
-    app = ttkb.Window(themename="superhero")
+    app = ttkb.Window(themename="simplex")
     app.title("Wolfkit")
     app.geometry("800x600")
     app.minsize(600, 400)
+
+    # Set custom wolfkit app icon
+    app.iconphoto(False, tk.PhotoImage(file="assets/wolfkit-icon.png"))
 
     frame = AppFrame(app)
     frame.pack(fill=BOTH, expand=YES)
